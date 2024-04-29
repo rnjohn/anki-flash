d0caafb9da1accf2db41ba4b749c1ac4af606e34
John Fontalvo <jefopa99@gmail.com>
2024-04-29 14:42:37 -0500

    feat: add flashcard model ts

--- app/models/flashcard.js
+++
@@ -1,8 +0,0 @@
-import mongoose from 'mongoose';
-
-const flashcardSchema = new mongoose.Schema({
-  front: String,
-  back: String,
-});
-
-export default flashcardSchema;
---
+++ app/models/flashcard.ts
@@ -0,0 +1,19 @@
+import mongoose from 'mongoose';
+
+interface IFlaschcard {
+  front: string;
+  back: string;
+}
+
+const flashcardSchema = new mongoose.Schema<IFlaschcard>({
+  front: {
+    type: String,
+    required: true
+  },
+  back: {
+    type: String,
+    required: true
+  },
+});
+
+export default mongoose.models.Flashcard || mongoose.model<IFlaschcard>('Flashcard', flashcardSchema);
